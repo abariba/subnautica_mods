c2a5a930212d54c40937196f9fe8dc3f4fff0695
abariba <0171359@student.roc-nijmegen.nl>
2018-07-06 12:00:46 +0200

    sercuritycoming d

--- sandsharkCamo/Main.cs
+++ sandsharkCamo/Main.cs
@@ -28,28 +28,28 @@ namespace sandsharkCamo
     
             var gameObject = __instance.gameObject;
             if(gameObject != null){
-                var model = gameObject.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SSGlobalTSR");
-                var model1 = gameObject.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SandSharkGEO");
+                //var model = gameObject.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SSGlobalTSR");
+                var model = gameObject.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SandSharkGEO");
                 var model2 = gameObject.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SandSharkGEO_LOD1");
                 var model3 = gameObject.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SandSharkGEO_LOD2");
                 var model4 = gameObject.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SandSharkGEO_LOD3");
-                var model5 = gameObject.FindChild("models").FindChild("sand_shark_01");
+                //var model5 = gameObject.FindChild("models").FindChild("sand_shark_01");
 
-                var skinnedRenderer = model.GetComponent<MeshRenderer>();
-                var skinnedRenderer1 = model1.GetComponent<MeshRenderer>();
-                var skinnedRenderer2 = model2.GetComponent<MeshRenderer>();
-                var skinnedRenderer3 = model3.GetComponent<MeshRenderer>();
-                var skinnedRenderer4 = model4.GetComponent<MeshRenderer>();
-                var skinnedRenderer5 = model5.GetComponent<MeshRenderer>();
+                //var skinnedRenderer = model.GetComponent<SkinnedMeshRenderer>();
+                var skinnedRenderer1 = model1.GetComponent<SkinnedMeshRenderer>();
+                var skinnedRenderer2 = model2.GetComponent<SkinnedMeshRenderer>();
+                var skinnedRenderer3 = model3.GetComponent<SkinnedMeshRenderer>();
+                var skinnedRenderer4 = model4.GetComponent<SkinnedMeshRenderer>();
+                //var skinnedRenderer5 = model5.GetComponent<SkinnedMeshRenderer>();
 
                 var texture = TextureUtils.LoadTexture(@"./QMods/sandsharkCamo/sandsharkCamoDiff.png");
 
-                skinnedRenderer.sharedMaterial.mainTexture = texture;
+               // skinnedRenderer.sharedMaterial.mainTexture = texture;
                 skinnedRenderer1.sharedMaterial.mainTexture = texture;
                 skinnedRenderer2.sharedMaterial.mainTexture = texture;
                 skinnedRenderer3.sharedMaterial.mainTexture = texture;
                 skinnedRenderer4.sharedMaterial.mainTexture = texture;
-                skinnedRenderer5.sharedMaterial.mainTexture = texture;
+               // skinnedRenderer5.sharedMaterial.mainTexture = texture;
             }
             else{
                 Console.WriteLine("[sandsharkCamo] Error: SkinnedMeshRenderer not found on component");
