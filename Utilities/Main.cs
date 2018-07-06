d817426ae9b5318aa636b9035e641f85c2397e82
AlexejheroYTB <alexejhero@gmail.com>
2018-07-06 11:18:15 +0300

    added StalkerCamo back, added utilities project

---
+++ Utilities/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utilities
+{
+    class Main
+    {
+    }
+}
+
+// This is your shared project
+
+// Shared project can be used as references by adding them in References > Add Reference 
+// and clicking "Shared Projects" in the left-hand-side panel
+
+// Unlike normal projects, shared projects can't be built (turned into a DLL) manually
+// Shared projects are built automatically when building other projects that require it,
+// and the two DLLs are merged
