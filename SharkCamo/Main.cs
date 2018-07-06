52564a962508565711bcef035dbb6b5f203a5d1c
abariba <0171359@student.roc-nijmegen.nl>
2018-07-07 00:13:18 +0200

    send

---
+++ SharkCamo/Main.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+using UnityEngine;
+using Utilities;
+
+
+namespace SharkCamo
+{
+    public class Main
+    {
+        public static void Patch()
+        {
+            var harmony = HarmonyInstance.Create("com.abariba.SharkCamo");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Console.WriteLine("[sandsharkCamo] Initialized");
+        }
+    }
+
+    [HarmonyPatch(typeof(SandShark))]
+    [HarmonyPatch("Start")]
+    internal class SandShark_Start
+    {
+        [HarmonyPostfix]
+        [HarmonyPriority(int.MinValue)]
+        static void Postfix(SandShark __instance)
+        {
+
+            var gameObject = __instance.gameObject;
+            if (gameObject != null)
+            {
+                //var model1 = gameObject.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SSGlobalTSR");
+                var model1 = gameObject.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SandSharkGEO");
+                //var model2 = gameObject.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SandSharkGEO_LOD1");
+                //var model3 = gameObject.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SandSharkGEO_LOD2");
+                //var model4 = gameObject.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SandSharkGEO_LOD3");
+                //var model5 = gameObject.FindChild("models").FindChild("sand_shark_01");
+
+                var skinnedRenderer1 = model1.GetComponent<SkinnedMeshRenderer>();
+                //var skinnedRenderer1 = model1.GetComponent<SkinnedMeshRenderer>();
+                //var skinnedRenderer2 = model2.GetComponent<SkinnedMeshRenderer>();
+                //var skinnedRenderer3 = model3.GetComponent<SkinnedMeshRenderer>();
+                //var skinnedRenderer4 = model4.GetComponent<SkinnedMeshRenderer>();
+                //var skinnedRenderer5 = model5.GetComponent<SkinnedMeshRenderer>();
+
+                var texture = TextureUtils.LoadTexture(@"./QMods/sandsharkCamo/sandsharkCamoDiff.png");
+
+                skinnedRenderer1.sharedMaterial.mainTexture = texture;
+                //skinnedRenderer1.sharedMaterial.mainTexture = texture;
+                //skinnedRenderer2.sharedMaterial.mainTexture = texture;
+                //skinnedRenderer3.sharedMaterial.mainTexture = texture;
+                //skinnedRenderer4.sharedMaterial.mainTexture = texture;
+                // skinnedRenderer5.sharedMaterial.mainTexture = texture;
+
+                Console.WriteLine("[sandsharkCamo] Running as intended![this is a message to see if the sandshark creature gets called properly]");
+            }
+            else
+            {
+                Console.WriteLine("[sandsharkCamo] Error: SkinnedMeshRenderer not found on component");
+            }
+
+
+        }
+
+    }
+}
---
+++ SharkStalkerCamo/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+using UnityEngine;
+using Utilities;
+
+
+namespace SharkStalkerCamo
+{
+    public class Main
+    {
+        public static void Patch()
+        {
+            var harmony = HarmonyInstance.Create("com.abariba.SharkStalkerCamo");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+        }
+    }
+
+    [HarmonyPatch(typeof(Creature), "Start")]
+    //[HarmonyPatch(typeof(Stalker),"Start")]
+
+//    you could workaround this
+//have a main mod that hooks Creature.Start but have it loop through a static public List<CreaturePatches>
+//you can then make QMods load it early and have your other mods load later on, again with QMods
+//there shouldn't be any problems referencing the main mod from your other mods(edited)
+
+
+
+
+
+    internal class SandSharkCamo_Start
+    {
+        [HarmonyPostfix]
+        [HarmonyPriority(int.MinValue)]
+        static void Postfix(Stalker __instance)
+        {
+
+            var gameObject1 = __instance.gameObject;
+
+
+
+            if (gameObject1 != null)
+            {
+                Console.WriteLine("[sandsharkCamo] Initialized");
+                var sandshark = gameObject1.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SandSharkGEO");
+                var model = gameObject1.FindChild("models").FindChild("sand_shark_01").FindChild("sand_shark_rig_SandSharkGEO");
+
+
+                var sandshark_skinnedRenderer = sandshark.GetComponent<SkinnedMeshRenderer>();
+
+
+                var texture = TextureUtils.LoadTexture(@"./QMods/SharkStalkerCamo/sandsharkCamoDiff.png");
+
+                sandshark_skinnedRenderer.sharedMaterial.mainTexture = texture;
+
+
+                Console.WriteLine("[sandsharkCamo] Running as intended![this is a message to see if the sandshark creature gets called properly!]");
+            }
+            else
+            {
+                Console.WriteLine("[sandsharkCamo] Error: SkinnedMeshRenderer not found on component");
+            }
+            if (gameObject1 != null)
+            {
+                Console.WriteLine("[stalkerCamo] Initialized");
+                var model2 = gameObject1.FindChild("Stalker_02").FindChild("snout_shark_geo");// .FindChild("Stalker_02").FindChild("snout_shark_geo");
+                //var model2 = gameObject2.FindChild("Stalker_02").FindChild("snout_shark_geo");
+                var skinnedRenderer2 = model2.GetComponent<SkinnedMeshRenderer>();
+                //if (rnd == 0)
+                //{
+                var texture2 = TextureUtils.LoadTexture(@"./QMods/SharkStalkerCamo/stalkerCamoDiff.png");
+                skinnedRenderer2.sharedMaterial.mainTexture = texture2;
+                Console.WriteLine("[stalkerCamo] Running as intended![this is a message to see if the stalker creature gets called properly]");
+            }
+            else
+            {
+                Console.WriteLine("[stalkerCamo] An unknown error occured. Stalker game object is null");
+            }
+
+            return;
+        }
+    }
+    
+
+
+
+}
